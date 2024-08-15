3b477c93209c816a54517c8873e6dc1ac5317aae
JuanOneD <Juandiego.jd42@gmail.com>
2024-08-15 11:26:33 -0300

    MAKE A LITTLE CRUD

---
+++ Projects/MiniCrude/Script.js
@@ -0,0 +1,14 @@
+function ChangeImg(){
+    document.getElementById("ImgLink").click();
+}
+function UpdateImg(){
+    let img = document.getElementById("UserImg");
+    
+    if (!document.getElementById("ImgLink").files[0] ||!document.getElementById("ImgLink").files[0].type.startsWith('image/')) {
+        return;
+    }
+
+    let reader = new FileReader();
+    reader.onload = (e)=>img.src = e.target.result;
+    reader.readAsDataURL(document.getElementById("ImgLink").files[0]);
+}
