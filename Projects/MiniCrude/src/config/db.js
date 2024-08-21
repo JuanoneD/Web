8747e9a9af882f2554a5dd3311def9ecb991a37f
Juan Diego <134559247+JuanoneD@users.noreply.github.com>
2024-08-21 19:55:06 -0300

    Delete Projects directory

--- Projects/MiniCrude/src/config/db.js
+++
@@ -1,8 +0,0 @@
-const sequelize = require('sequelize');
-//configurações da base de dados
-const database = new sequelize('juanWeb', 'JuanWeb', 'Juan@23Q',
-{
-    dialect: 'mssql', host:'localhost', port: 49928
-});
-database.sync();
-module.exports = database;
